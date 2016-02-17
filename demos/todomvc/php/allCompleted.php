9de501a5098ab209b326cf1726044597433a6592
Intersel <github@intersel.org>
2016-02-17 22:55:17 +0100

    fix demo todomvc

---
+++ demos/todomvc/php/allCompleted.php
@@ -0,0 +1,18 @@
+<?php
+
+$getAction = 'allCompleted';
+$toggleStatus = ($_REQUEST['toggleStatus']=='true')?true:false;
+$toggleStatusName = $toggleStatus?'On':'Off';
+$toggleChecked = $toggleStatus?'checked':'';
+
+$returnStr = <<<EOD
+<input class="toggle-all"
+		type="checkbox"
+		$toggleChecked
+		data-blapy-container="true"
+		data-blapy-container-name="selectAllToggle"
+		data-blapy-container-content="selectAllToggle-$toggleStatusName"
+		onclick="$('#myBlapy').trigger('postData',{aUrl:'php/allCompleted.php',params:{toggleStatus:$(this).prop('checked')}})">
+EOD;
+
+include ('getTodo.php');
