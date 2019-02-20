dd5367b9458bb9894830c8f353be6dc4e391d4f9
intersel <github@intersel.org>
2019-02-20 14:42:17 +0100

    possible to get the emails list from txt file

--- demos/verifyEmails/emailsList.php
+++ demos/verifyEmails/emailsList.php
@@ -1,13 +1,41 @@
-<div id="results"
-	data-blapy-container="true"
-	data-blapy-container-name="results"
+<?php
+/*
+
+email files with a email per line
+
+*/
+$emailList = array();
+$emailListJson ='';
+
+if (array_key_exists('file',$_GET)) 
+{
+	$filename=$_GET['file'];
+	if (file_exists($filename)) $emailList = file($filename,FILE_SKIP_EMPTY_LINES);
+	if (count($emailList)>0)
+	{
+		foreach($emailList as $aEmail)
+		{
+			$emailListJson .= "{email: \"".trim($aEmail,"\n")."\"},\n";
+		}
+		$emailListJson = trim(trim($emailListJson,"\n"),',');
+	}
+}
+
+if (empty($emailListJson)) $emailListJson ='
+{email: "emmanuel.podvin@intersel.fr"},
+{email: "em.blapy@yahoo.com"},
+{email: "blapy"},
+{email: "blapy@yahoo"},
+{email: "blapy@yahoo.fr.com"}
+';
+
+?>
+<div id="results" 
+	data-blapy-container="true" 
+	data-blapy-container-name="results" 
 	data-blapy-container-content="resultsnew"<?php echo time();?>
 >
 [
-	{"email": "emmanuel.podvin@intersel.fr"},
-	{"email": "em.blapy@yahoo.com"},
-	{"email": "blapy"},
-	{"email": "blapy@yahoo"},
-	{"email": "blapy@yahoo.fr.com"}
+<?php echo $emailListJson."\n"; ?>
 ]
 </div>
--- demos/verifyEmails/index.php
+++ demos/verifyEmails/index.php
@@ -2,6 +2,12 @@
 <!-- To run the current sample code in your own environment, copy this to an html page. -->
 <html id="myBlapy">
 <?php
+/*
+index.php
+
+index.php?file=localfilename.csv
+*/
+
 $defaultTitle = "Email tester";
 include("header.php");
 ?>
@@ -60,7 +66,7 @@ include("header.php");
 		data-blapy-update="json"
 		data-blapy-template-file="resultValues.tpl"
 		data-blapy-template-wrap="<table>"
-		data-blapy-template-init="emailsList.php"
+		data-blapy-template-init="emailsList.php<?php if (array_key_exists('file',$_GET)) echo "?file=".$_GET['file'];?>"
 	>
 	</div>	
 </body>
